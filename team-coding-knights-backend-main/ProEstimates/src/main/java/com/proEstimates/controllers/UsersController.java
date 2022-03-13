090edc5baf57090ff542a25baa821432d380247a
Brian DeWitt <80775722+slugguts@users.noreply.github.com>
2022-03-12 18:44:41 -0800

    Brian's initial commit
    Added packages and initialized classes

---
+++ team-coding-knights-backend-main/ProEstimates/src/main/java/com/proEstimates/controllers/UsersController.java
@@ -0,0 +1,22 @@
+package com.proEstimates.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ProEstimates.models.User;
+
+@Controller
+@RequestMapping({"/","/users"})
+public class UsersController {
+	@GetMapping()
+	public String getAllUsers (Model model) {
+		List<User> users = new ArrayList<User>();
+		model.addAttribute("users", users);
+		return "users";
+	}
+}
---
+++ team-coding-knights-backend-main/ProEstimates/src/main/java/com/proEstimates/models/Materials.java
@@ -0,0 +1,40 @@
+package com.proEstimates.models;
+
+
+import java.sql.Date;
+
+
+public class Materials {
+	private int material_id;
+	private String material_name;
+	private int material_price;
+	private Date material_set_date;
+	
+	public int getMaterial_id() {
+		return material_id;
+	}
+	public void setMaterial_id(int material_id) {
+		this.material_id = material_id;
+	}
+	public String getMaterial_name() {
+		return material_name;
+	}
+	public void setMaterial_name(String material_name) {
+		this.material_name = material_name;
+	}
+	public int getMaterial_price() {
+		return material_price;
+	}
+	public void setMaterial_price(int material_price) {
+		this.material_price = material_price;
+	}
+	public Date getMaterial_set_date() {
+		return material_set_date;
+	}
+	public void setMaterial_set_date(Date material_set_date) {
+		this.material_set_date = material_set_date;
+	}
+	
+
+
+}
